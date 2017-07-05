23fd2da774ded0cee6a48d2cf42620970169545f
U-MPLS\hbowden <2trill2spill@gmail.com>
2017-07-05 10:53:06 -0500

    Add a npm run analyze cli option.

---
+++ webpack.analyzer.config.js
@@ -0,0 +1,35 @@
+
+var path = require('path');
+var webpack = require("webpack");
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  entry: [
+    './src/app.js'
+  ],
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'public/scripts'),
+    publicPath: '/public/scripts'
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.js)$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css-loader?modules&importLoaders=1&localIdentName=ra_[name]__[local]' 
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new BundleAnalyzerPlugin()
+  ]
+}
